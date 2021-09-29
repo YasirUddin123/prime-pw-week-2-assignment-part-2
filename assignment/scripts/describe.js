72f25df48615be4adc7d55efdf85f19669a00374
Yasir Uddin <yasir.uddin@icloud.com>
2021-09-29 10:40:11 -0700

    updated assignment week 2 part 2

--- assignment/scripts/describe.js
+++ assignment/scripts/describe.js
@@ -1,6 +1,6 @@
 // WHAT IS THIS EVEN DOING?
 
-// For questions 1-3, each code block is syntactically and logically correct. Your job is to describe what is happening. 
+// For questions 1-3, each code block is syntactically and logically correct. Your job is to describe what is happening.
 // They are in isolation -- variables in one question do not affect other questions.
 // Describe the code as it would be run, and make sure you give us the console.log's value at the end.
 // (Don't just say 'we console log the variable' -- instead say something like 'we console log 'yes'')
@@ -26,10 +26,10 @@ if (number >= 2) {
 
 
 // 1. WRITE YOUR DESCRIPTION HERE
-// 
-// 
-// 
-//
+// We create a variable called 'name' and we assign it a value called 'Dane'.
+// Then we check to see if 'Dane' is exactly equal to 'Mary'.
+// If 'Dane' and 'Mary' are exactly equal, then we console log 'Hi, Mary!'.
+//Otherwise, we console log 'How do you do?'
 
 //CODE
 /*
@@ -45,10 +45,10 @@ if (name === 'Mary') {
 */
 
 // 2. WRITE YOUR DESCRIPTION HERE
-// 
-// 
-// 
-//
+// We create a variable called 'secret', but we do not define 'secret'.
+// We create a variable called 'code' and assign it a value with the number 123.
+// We check to see if the value of 'code' is exactly equal to 123. If true, we assign the value 'super' to the variable 'secret'. Then we take the value of 'code', multiply it by 2, and assign this multiplication to 'code'.
+// We also check to see if the value of 'code' is greater than 250. If this is true, we assign the value 'duper' to the variable 'secret'. Then we console log 'secret'.
 
 //CODE
 /*
@@ -70,10 +70,11 @@ console.log(secret)
 */
 
 // 3. WRITE YOUR DESCRIPTION HERE
-// 
-// 
-// 
-//
+// We create a variable called 'isStudent' and assign it the boolean value true. We create a variable called 'age' and assign it the number 34. And we create the variable called 'zip' and assign it the number 55407.
+// We check four conditions. First, we check if both the variable 'isStudent' is true and if the variable 'zip' is greater than 8000. If both evaluate to true, we console log `You're a student on the West Coast!`.
+// Otherwise, we then check to see if either the variable 'isStudent' is false or the variable 'age' is less than 30. If either of these are true, we console log 'What are your hobbies?'.
+//Otherwise, we check if the variable 'isStudent' is true. If that is true, then we console log 'Welcome to Prime!'.
+// But if none of these conditions are true, then we console log 'How about the weather?'.
 
 //CODE
 /*
@@ -97,7 +98,7 @@ if (isStudent === true && zip > 80000 ) {
 
 
 // REVERSE
-// Now, for questions 4-6, we'll give you a description and some code. 
+// Now, for questions 4-6, we'll give you a description and some code.
 // Some of the code will NOT match the description.
 // Comment above the problem code, and describe a potential solution.
 // Look closely -- things are logically incorrect or missing and wrong in the code!
@@ -106,16 +107,16 @@ if (isStudent === true && zip > 80000 ) {
 // 0. (DESCRIPTION OF CORRECT BEHAVIOR)
 // We make a variable called number and set it to 1 as a number.
 // Then we increment the number variable. Number is now 2.
-// We check if number is greater than or equal to 2. 
+// We check if number is greater than or equal to 2.
 // -- it is, so we console.log 'yes'
 
 //CODE
 /*
 let number = 1;
 
-// FIX - number-- decrements number, but the instructions ask to increment. 
+// FIX - number-- decrements number, but the instructions ask to increment.
 // Should be number++.
-number--; 
+number--;
 
 if (number >= 2) {
   console.log('yes');
@@ -125,45 +126,62 @@ if (number >= 2) {
 
 
 
-//4. We start with three variables -- colorOne is set to 'blue' and 
-// colorTwo is set to 'red', and mix is set to true. We check if mix is true 
+//4. We start with three variables -- colorOne is set to 'blue' and
+// colorTwo is set to 'red', and mix is set to true. We check if mix is true
 // -- it is, so we set colorOne and colorTwo to 'purple'
 
 /*
 let colorOne = 'red';
+//FIX - colorOne is not set to 'blue'.
+//colorOne should be set to 'blue'.
 let colorTwo = 'blue';
+//FIX - colorTwo is not set to 'red'.
+//colorOne should be set to 'red'.
 let mix = true;
 
 if (mix === true) {
   colorOne = 'purple';
+  //FIX - ColorTwo is not included and needs to be set to 'purple'.
+  //insert ColorTwo here and set to 'purple'.
+
 }
 */
 
 //5. We start with two variables -- temp is set to 40 and time is set to 4.
-// We check if temp is higher than 39 and if time is greater or equal to  4 
+// We check if temp is higher than 39 and if time is greater or equal to  4
 // -- they are so we console.log 'throw away the food!'
 
 /*
 let temp = 40;
 const time = 4;
-
+//FIX - we are setting time as a constant, not a variable.
+//We should use the word 'let' instead of 'const' to create the 'time' variable.
 if (temp > 39 || time >= 4) {
+//FIX - we are not using the correct compound conditional
+// We should use && instead of ||
   console.log('throw away the food!');
 }
 */
 
 //6. We start with two variables -- age is set to 21 and minAge is set to 21.
-// We check if age is greater than or equal to minAge 
+// We check if age is greater than or equal to minAge
 // -- it is, so we console.log 'enter'.
 
 /*
 let age = 21;
 const minAge = 21;
+//FIX - we are setting minAge as a constant, not a variable.
+//We should use the word 'let' instead of 'const' to create the 'minAge' variable
 
 if(minAge <= age) {
+//FIX - this is saying if minAge is less than or equal to minAge. This is not what we want.
+//Better to use age >= minAge in our conditional
   console.log('no entry');
+//FIX - this is not relevant on what's asked
+//Update the console log to say 'enter' assuming the first FIX is made.
 } else {
   console.log('enter');
+//FIX - We don't need this if we made the two fixes earlier.
+// Delete this else statement
 }
 */
-
